391c5f983e201385bdd18b7ad8fc750a59cc1e7c
Rami B <ramibasheer9@gmail.com>
2022-06-29 14:29:16 +0200

    submitting changes

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
-// write your code in this file
+console.log("This is an exercise in console logging");
+
+let exercise = "This is the value.";
+console.log(exercise);
+
+let firstName = "Rami";
+let lastName = "Basheer";
+console.log(firstName, lastName);
+
+let city = "Berlin";
+let job = "Web Developer";
+let age = 27 ;
+console.log(firstName, lastName, age, city, job);
+
+console.log(firstName + " is a " + age + " years old " + job + " who lives in " + city);
+
+
+
